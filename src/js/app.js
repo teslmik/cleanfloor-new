11e09e98e1275f3bb8e7d4e33e2c179922b12a86
Teslmik <teslmik@gmail.com>
2022-11-03 21:15:31 +0200

    Slider

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,5 @@ import * as flsFunction from './modules/functions.js';
 import { da } from './modules/dynamicAdapt.js';
 
 flsFunction.isWebp();
-// flsFunction.swiper();
-// flsFunction.ibg();
 da.init();
 
